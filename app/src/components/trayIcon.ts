12248d37157cfca640cbe6876ab0581a139cab40
thamerx <ithamersz@gmail.com>
2020-12-06 12:48:31 +0300

    Windows Support for taskbar + tray badges
    Windows Support for taskbar + tray badges
    
    based on
    Radomír Polách feature: tray icon notifications
    https://github.com/exander77/nativefier/commit/be2a6a435bfc8deb4e0563afbf95892e3c0f27f4

--- app/src/components/trayIcon.ts
+++ app/src/components/trayIcon.ts
@@ -39,20 +39,47 @@ export function createTrayIcon(
         const counterValue = getCounterValue(title);
         if (counterValue) {
           appIcon.setToolTip(`(${counterValue})  ${options.name}`);
+
+          let nimage = nativeImage.createFromPath(iconPath+"."+counterValue+".ico");
+
+          if (Number(counterValue)  < 1000)
+          {
+            mainWindow.setIcon(iconPath+"."+counterValue+".ico");
+            nimage = nativeImage.createFromPath(iconPath+"."+counterValue+".ico");
+          }else
+          {
+            nimage = nativeImage.createFromPath(iconPath+"."+1000+".ico");
+            mainWindow.setIcon(iconPath+"."+1000+".ico");
+          }
+          
+          appIcon.setImage(nimage);
         } else {
           appIcon.setToolTip(options.name);
+          const nimage = nativeImage.createFromPath(iconPath);
+          appIcon.setImage(nimage);
+          mainWindow.setIcon(iconPath);
         }
       });
     } else {
+      let counterValue = 0;
+
       ipcMain.on('notification', () => {
         if (mainWindow.isFocused()) {
           return;
         }
         appIcon.setToolTip(`•  ${options.name}`);
+        counterValue = Math.min(counterValue + 1, 1000);
+        const nimage = nativeImage.createFromPath(iconPath+"."+counterValue+".ico");
+        appIcon.setImage(nimage);
+        mainWindow.setIcon(iconPath+"."+counterValue+".ico");
       });
 
       mainWindow.on('focus', () => {
         appIcon.setToolTip(options.name);
+        counterValue = 0;
+        const nimage = nativeImage.createFromPath(iconPath);
+        appIcon.setImage(nimage);
+        mainWindow.setIcon(iconPath);
       });
     }
 
